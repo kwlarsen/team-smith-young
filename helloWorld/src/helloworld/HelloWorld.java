e53c8b0a95a3f7dca7a7190f8782ae183eab20bf
lar08 <lar08@172.16.225.129>
2016-05-12 09:32:06 -0700

    This is my first commit that I will be practicing

---
+++ helloWorld/src/helloworld/HelloWorld.java
@@ -0,0 +1,16 @@
+package helloworld;
+import ClassesInProject.hellowWorldOnRoids;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+       
+        hellowWorldOnRoids newHelloRoid = new hellowWorldOnRoids();
+        
+        System.out.println("Java print line");
+        System.out.println(newHelloRoid.firstdouble);
+        
+    }
+
+    
+}
